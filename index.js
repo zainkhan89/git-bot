f4b937f604449ed4f838c1f012ca8985e983b46a
Zain Ul Abideen Khan <zua.khan89@gmail.com>
2023-10-26 14:32:19 +0500

    update

--- index.js
+++ index.js
@@ -4,28 +4,40 @@ const simpleGit = require("simple-git");
 
 const FILE_PATH = "./data.json";
 
-const getRandomInt = (min, max) => {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.floor()*(max-min+1)) + min;
-};
+// const getRandomInt = (min, max) => {
+//     min = Math.ceil(min);
+//     max = Math.floor(max);
+//     return Math.floor(Math.floor()*(max-min+1)) + min;
+// };
 
-const makeCommit = n => {
-    if (n===0) return simpleGit().push();
-    const DATE = moment ()
-    .subtract(getRandomInt(0, 365), "days")
-    .format();
+// const makeCommit = n => {
+//     if (n===0) return simpleGit().push();
+//     const DATE = moment ()
+//     .subtract(getRandomInt(0, 365), "days")
+//     .format();
 
-    const date = {
-        date: DATE
-    };
-    console.log(n);
+//     const date = {
+//         date: DATE
+//     };
+//     console.log(n);
 
-    jsonfile.writeFile( FILE_PATH, date, ()=>{
-        simpleGit()
-        .add([FILE_PATH])
-        .commit( DATE, {"--date":DATE}, makeCommit.bind(this, --n) )
-    })
+//     jsonfile.writeFile( FILE_PATH, date, ()=>{
+//         simpleGit()
+//         .add([FILE_PATH])
+//         .commit( DATE, {"--date":DATE}, makeCommit.bind(this, --n) )
+//     })
+// }
+
+// makeCommit(100);
+
+
+const DATE = moment().subtract(5, 'd').format();
+const data = {
+    date: DATE
 }
 
-makeCommit(100);
+console.log(data);
+console.log(DATE);
+
+jsonfile.writeFile(FILE_PATH, data);
+simpleGit().add([FILE_PATH]).commit(DATE, {'--date': DATE}).push();

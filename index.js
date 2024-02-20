16904d76ad52fc6e420f15e960b7280d614463f8
Zain Ul Abideen Khan <98710568+omerch54@users.noreply.github.com>
2024-02-20 19:02:45 +0500

    update

--- index.js
+++ index.js
@@ -6,32 +6,32 @@ const fs = require("fs");
 const FILE_PATH = "./data.json";
 
 const getRandomInt = (min, max) => {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min + 1)) + min;
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 };
 
-const makeCommit = n => {
-    if (n === 0) return simpleGit().push();
+const makeCommit = (n) => {
+  if (n === 0) return simpleGit().push();
 
-    const DATE = moment()
-        .subtract(getRandomInt(0, 365), "days")
-        .startOf('day')
-        .format();
+  const DATE = moment()
+    .subtract(getRandomInt(0, 730), "days")
+    .startOf("day")
+    .format();
 
-    const data = {
-        date: DATE
-    };
+  const data = {
+    date: DATE,
+  };
 
-    console.log(n);
-    console.log(data);
-    console.log(DATE);
+  console.log(n);
+  console.log(data);
+  console.log(DATE);
 
-    fs.appendFile(FILE_PATH, JSON.stringify(data) + "\n", () => {
-        simpleGit()
-            .add([FILE_PATH])
-            .commit(DATE, { "--date": DATE }, makeCommit.bind(this, --n));
-    });
+  fs.appendFile(FILE_PATH, JSON.stringify(data) + "\n", () => {
+    simpleGit()
+      .add([FILE_PATH])
+      .commit(DATE, { "--date": DATE }, makeCommit.bind(this, --n));
+  });
 };
 
-makeCommit(2000);
+makeCommit(1500);
